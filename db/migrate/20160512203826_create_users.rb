7e3a859749ae318400f6fd22f9e7c32dc704764d
CarlinCharge <sprintman210@gmail.com>
2016-05-12 16:57:51 -0400

    adds user models and migrations

---
+++ db/migrate/20160512203826_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :first_name, null:false
+      t.string :last_name, null: false
+      t.string :password_digest, null: false
+      t.string :email, null: false, unique: true
+      t.integer :phone, null: false, unique: true
+      t.string :location, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
