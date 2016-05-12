23d9f7e94305c8c76d6066d2cdf50c95a8e7cbbb
Alex Forger & Jillian Campbell <pair+darrow87.jillwc07@devbootcamp.com>
2016-05-12 19:32:25 -0400

    add event handling on google maps click to obtain latitude and longitude

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
+  root 'welcome#index'
+
+
   get "logout" => "sessions#destroy", as: "logout"
   get "login" => "sessions#new", as: "login"
   post "login" => "sessions#create", as: "new_login"
 
   resources :users, only: [:show, :new, :create]
 
-  root "users#new"
+
 end
