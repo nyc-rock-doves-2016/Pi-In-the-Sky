b2f177ef32c6337f43dfbbf3dc1634c26923e560
CarlinCharge <sprintman210@gmail.com>
2016-05-17 10:50:38 -0400

    adds 404 error handling for not being logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-
-
-
-
   protect_from_forgery with: :null_session
+  
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    session[:user_id] && current_user.present?
+  end
 
 
   GOOGLE_API_KEY = ENV["GOOGLE_API_KEY"]
