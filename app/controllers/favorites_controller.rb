907697f2b57213727e4bd932c0822406b51f57d4
Alex Forger <alexforgerii@gmail.com>
2016-05-18 19:44:41 -0400

    style login page and fix bug that hid user profile button when same city was favorited twice

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,10 +1,10 @@
 class FavoritesController < ApplicationController
 
   def create
-    user = User.find_by(id: session[:user_id])
+    @user = User.find_by(id: session[:user_id])
     global_data_object = GlobalDataObject.last
     location = global_data_object.city
-    if user.global_data_objects.find_by(city: location) == nil
+    if @user.global_data_objects.find_by(city: location) == nil
       @favorite = Favorite.create(user_id: user.id, global_data_object_id: global_data_object.id)
       redirect_to global_data_objects_path
     else
