1d5d9cb02b224c45952f008d5ba41e4a2c82bb41
CarlinCharge <sprintman210@gmail.com>
2016-05-19 16:24:14 -0400

    change routing path to go to global data objects path on login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to user_path(user)
+      redirect_to global_data_objects_path
     else
       @errors = ['Invalid Email or Password']
       render 'new'
