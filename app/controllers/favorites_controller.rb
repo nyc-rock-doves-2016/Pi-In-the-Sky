2a11ea08290715a779f19759836dfc9fd0a196dc
Alex Forger <alexforgerii@gmail.com>
2016-05-19 11:17:31 -0400

    Add styling to user show, login, homepage, nav form, and GDO index page.

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,7 +5,7 @@ class FavoritesController < ApplicationController
     global_data_object = GlobalDataObject.last
     location = global_data_object.city
     if @user.global_data_objects.find_by(city: location) == nil
-      @favorite = Favorite.create(user_id: user.id, global_data_object_id: global_data_object.id)
+      @favorite = Favorite.create(user_id: @user.id, global_data_object_id: global_data_object.id)
       redirect_to global_data_objects_path
     else
       @errors = ["You have already favorited this city!"]
